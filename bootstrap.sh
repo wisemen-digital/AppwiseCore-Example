e2d76c7f7dcf3b7507b51ab19fdd0c6011aab790
David Jennes <david.jennes@appwise.be>
2018-05-04 03:03:06 +0200

    Update the bootstrap script to be able to fix the podfile

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,6 +15,69 @@ while [ -z "$appleID" ]; do
      read -p "Please provide your Apple ID (your.email@address.com): " appleID
 done
 
+#### Steps ####
+
+function deintegratePods {
+	command -v pod >/dev/null 2>&1 || { echo >&2 "I require CocoaPods but it's not installed.  Aborting."; exit 1; }
+	echo "Deintegrating pods..."
+	pod deintegrate >/dev/null
+}
+
+function fixPodfile {
+	echo "Fixing Podfile..."
+
+	sed -i.bak -e "s/, :path => '\.\.\/'//g" Podfile && rm Podfile.bak
+	sed -i.bak -e "s/\\\"\.\.\//\\\"\\\${PODS_ROOT}\/AppwiseCore\//g" Podfile && rm Podfile.bak
+}
+
+# move files to new locations, we handle up to 2 levels deep of renaming
+function relocateFiles {
+	echo "Moving files..."
+
+	find . -path "$podsDir" -prune -o -name "*${oldName}*" -print0 | while IFS= read -r -d '' file; do
+		target="${file/$oldName/$newName}"
+
+		if [[ $target = *"$oldName"* ]]; then
+			# already moved the parent
+			deeperTarget="${target/$oldName/$newName}"
+			mv "$target" "$deeperTarget"
+		else
+			mv "$file" "$target"
+		fi
+	done
+}
+
+function replaceText {
+	echo "Replacing '$1' with '$2'..."
+
+	grep -rl --null "$1" --exclude-dir="$podsDir" --exclude="rename.sh" . | xargs -0 sed -i '' "s/$1/$2/g"
+}
+
+function configureFastlane {
+	echo "Creating Fastlane environment file..."
+
+	cat >.env <<EOL
+# Your Apple email address
+USER_APPLE_ID=$appleID
+EOL
+}
+
+function podInstall {
+	command -v pod >/dev/null 2>&1 || { echo >&2 "I require CocoaPods but it's not installed.  Aborting."; exit 1; }
+	echo "Installing pods..."
+	pod install >/dev/null
+}
+
+function cleanup {
+	echo "Cleaning up..."
+	rm -f "CHANGELOG.md"
+	rm -f "LICENSE"
+	rm -f "README.md"
+	rm -f "bootstrap.sh"
+}
+
+#### Start of flow ####
+
 # constants
 podsDir="./Pods"
 oldName="Example Project"
@@ -33,49 +96,17 @@ echo "- Bundle: $newBundle"
 echo "- Apple ID: $appleID"
 read -rsn1 -p "Press any key to continue";echo
 
-# pod deintegrate
-echo "Deintegrating CocoaPods before renaming..."
-command -v pod >/dev/null 2>&1 || { echo >&2 "I require CocoaPods but it's not installed.  Aborting."; exit 1; }
-pod deintegrate >/dev/null
-
-# move files to new locations, we handle up to 2 levels deep of renaming
-echo "Moving files..."
-find . -path "$podsDir" -prune -o -name "*${oldName}*" -print0 | while IFS= read -r -d '' file; do
-	target="${file/$oldName/$newName}"
-
-	if [[ $target = *"$oldName"* ]]; then
-		# already moved the parent
-		deeperTarget="${target/$oldName/$newName}"
-		mv "$target" "$deeperTarget"
-	else
-		mv "$file" "$target"
-	fi
-done
+deintegratePods
+fixPodfile
+relocateFiles
 
 # replace name in files
-echo "Replacing '$oldName' with '$newName'..."
-grep -rl --null "$oldName" --exclude-dir="$podsDir" --exclude="rename.sh" . | xargs -0 sed -i '' "s/$oldName/$newName/g"
+replaceText "$oldName" "$newName"
+replaceText "$oldModuleName" "$newModuleName"
+replaceText "$oldBundle" "$newBundle"
 
-echo "Replacing '$oldModuleName' with '$newModuleName'..."
-grep -rl --null "$oldModuleName" --exclude-dir="$podsDir" --exclude="rename.sh" . | xargs -0 sed -i '' "s/$oldModuleName/$newModuleName/g"
-
-echo "Replacing '$oldBundle' with '$newBundle'..."
-grep -rl --null "$oldBundle" --exclude-dir="$podsDir" --exclude="rename.sh" "." | xargs -0 sed -i '' "s/$oldBundle/$newBundle/g"
-
-# configure fastlane environment
-echo "Creating Fastlane environment file..."
-cat >.env <<EOL
-# Your Apple email address
-USER_APPLE_ID=$appleID
-EOL
-
-# cleanup
-echo "Cleaning up..."
-rm -f "CHANGELOG.md"
-rm -f "LICENSE"
-rm -f "README.md"
-rm -f "bootstrap.sh"
+configureFastlane
+podInstall
+cleanup
 
 echo "Done!"
-echo
-echo "You can now run 'pod install' and get started!"
