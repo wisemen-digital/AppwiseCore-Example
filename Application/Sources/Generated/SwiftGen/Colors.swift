47ee273bbfbf38af7ddd4bdb6d818862ddec45ad
David Jennes <david.jennes@gmail.com>
2019-03-05 14:44:21 +0100

    Pod update

--- Application/Sources/Generated/SwiftGen/Colors.swift
+++ Application/Sources/Generated/SwiftGen/Colors.swift
@@ -1,5 +1,5 @@
 // swiftlint:disable all
-// Generated using SwiftGen, by O.Halligon — https://github.com/SwiftGen/SwiftGen
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
 
 #if os(OSX)
   import AppKit
@@ -17,16 +17,16 @@
 // swiftlint:disable identifier_name line_length type_body_length
 internal extension ColorName {
   /// 0xd8011bff (r: 216, g: 1, b: 27, a: 255)
-  static let barRed = #colorLiteral(red: 0.84705883, green: 0.003921569, blue: 0.105882354, alpha: 1.0)
+  internal static let barRed = #colorLiteral(red: 0.84705883, green: 0.003921569, blue: 0.105882354, alpha: 1.0)
   /// 0x16182aff (r: 22, g: 24, b: 42, a: 255)
-  static let darkBlue = #colorLiteral(red: 0.08627451, green: 0.09411765, blue: 0.16470589, alpha: 1.0)
+  internal static let darkBlue = #colorLiteral(red: 0.08627451, green: 0.09411765, blue: 0.16470589, alpha: 1.0)
   /// 0xb9353bff (r: 185, g: 53, b: 59, a: 255)
-  static let darkRed = #colorLiteral(red: 0.7254902, green: 0.20784314, blue: 0.23137255, alpha: 1.0)
+  internal static let darkRed = #colorLiteral(red: 0.7254902, green: 0.20784314, blue: 0.23137255, alpha: 1.0)
   /// 0x979797ff (r: 151, g: 151, b: 151, a: 255)
-  static let fadedBlue = #colorLiteral(red: 0.5921569, green: 0.5921569, blue: 0.5921569, alpha: 1.0)
+  internal static let fadedBlue = #colorLiteral(red: 0.5921569, green: 0.5921569, blue: 0.5921569, alpha: 1.0)
   /// 0xedf2f4ff (r: 237, g: 242, b: 244, a: 255)
-  static let lightGray = #colorLiteral(red: 0.92941177, green: 0.9490196, blue: 0.95686275, alpha: 1.0)
+  internal static let lightGray = #colorLiteral(red: 0.92941177, green: 0.9490196, blue: 0.95686275, alpha: 1.0)
   /// 0xde293fff (r: 222, g: 41, b: 63, a: 255)
-  static let red = #colorLiteral(red: 0.87058824, green: 0.16078432, blue: 0.24705882, alpha: 1.0)
+  internal static let red = #colorLiteral(red: 0.87058824, green: 0.16078432, blue: 0.24705882, alpha: 1.0)
 }
 // swiftlint:enable identifier_name line_length type_body_length
