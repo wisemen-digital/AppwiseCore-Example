782329e0d6bfa9e424705c85e42a3b22d4ae9f9d
Tom Knapen <tom@knapen.io>
2020-10-29 12:02:46 +0100

    add example showing how to style navigation bar starting from iOS 13

--- Application/Sources/Application Services/StyleApplicationService.swift
+++ Application/Sources/Application Services/StyleApplicationService.swift
@@ -13,10 +13,22 @@ final class StyleApplicationService: NSObject, ApplicationService {
 	// swiftlint:disable:next discouraged_optional_collection
 	func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
 		UINavigationBar.appearance(whenContainedInInstancesOf: [NavigationController.self]).do {
+			if #available(iOS 13.0, *) {
+				let appearance = UINavigationBarAppearance().then {
+					$0.backgroundColor = Asset.Style.background.color
+					$0.shadowColor = .clear
+				}
+
+				$0.standardAppearance = appearance
+				$0.compactAppearance = appearance
+				$0.scrollEdgeAppearance = appearance
+			} else {
+				$0.barTintColor = Asset.Style.background.color
+				$0.shadowImage = UIImage()
+			}
+
 			$0.barStyle = .black
-			$0.barTintColor = Asset.Style.background.color
 			$0.isTranslucent = false
-			$0.shadowImage = UIImage()
 		}
 
 		StateEmptyView.appearance().do {
